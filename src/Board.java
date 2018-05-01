a8cbe23ae959d3525128b6b1f22cfcdafd11664b
penvarict <penvarict@s.dcsdk12.org>
2018-05-01 11:05:29 -0600

    finish answers

--- src/Board.java
+++ src/Board.java
@@ -165,6 +165,23 @@ public abstract class Board {
 	 */
 	public abstract boolean isLegal(List<Integer> selectedCards);
 
+	public boolean isLegal(List<Integer> selectedCards){
+		int card;
+		if (selectedCards.size >= 3){
+			for (i=0; i< selectedCards.size; i++){
+				card = selectedCards.get(i);
+				if(card != 10){ return false;}
+				else return true;
+
+			}
+
+		}else if(selectedCards.size = 2 && selectedCards.get(i)+selectedCards(i+1)= 11)  {
+				return true;
+		}else return false;
+
+
+	}
+
 	/**
 	 * Method to be completed by the concrete class that determines
 	 * if there are any legal plays left on the board.
@@ -172,6 +189,56 @@ public abstract class Board {
 	 *         false otherwise.
 	 */
 	public abstract boolean anotherPlayIsPossible();
+
+	public boolean anotherPlayIsPossible(){
+		int size = cards.length();
+		int numberofFaceCards = 0;
+
+		for (i=0; i< size; i++){
+			if (cards[i] = 10){
+				numberofFaceCards ++;
+			}
+			else if (numberofFaceCards = 3){
+				return true;
+			}
+		}
+		// for (i=0; i< size; i++){
+		// 	for (i=0; i< size; i++){
+		// 		for (i=0; i< size; i++){
+		// 			for (i=0; i< size; i++){
+		// 				for (i=0; i< size; i++){
+		// 					for (i=0; i< size; i++){
+		// 						for (i=0; i< size; i++){
+		// 							for (i=0; i< size; i++){
+		// 								for (i=0; i< size; i++){
+											
+											
+		// 								}
+										
+		// 							}	
+									
+		// 						}	
+								
+		// 					}
+		// 				}
+		// 			}	
+		// 		}
+		// 	}
+		// }
+
+		for (i=0; i< size; i++){ // card being compared, has to move by one everytieme
+			int h = 0;
+			for (h=0; h<size; h++){ //always has to start at zero 
+				if ( card[i]+card[h] = 11){ 
+					return true;
+				}
+
+			}
+
+		}return false;
+
+
+	}
 
 	/**
 	 * Deal cards to this board to start the game.
